60dd21ad3f782dc561d50a95783457335530bbfe
Centeno448 <diegocenteno448@gmail.com>
2026-03-19 18:35:11 -0500

    add missing test case

--- src/webcrawler.service.spec.ts
+++ src/webcrawler.service.spec.ts
@@ -10,18 +10,36 @@ import { Post } from './interfaces/post';
 describe('PostService', () => {
   let httpMock: DeepMockProxy<HttpService>;
   let webScraperService: WebCrawlerService;
+  let fixtureContent: string;
+
+  const expectedPosts: Post[] = [
+    { number: 1, title: 'AI Coding Is Gambling', points: 58, comments: 36 },
+    { number: 2, title: 'Death to Scroll Fade', points: 227, comments: 113 },
+    {
+      number: 3,
+      title: 'Snowflake AI Escapes Sandbox and Executes Malware',
+      points: 146,
+      comments: 38,
+    },
+    {
+      number: 4,
+      title: 'Show HN: Will my flight have Starlink?',
+      points: 0,
+      comments: 21,
+    },
+    {
+      number: 5,
+      title: 'A tiny, decentralised tool to explore the small web',
+      points: 88,
+      comments: 0,
+    },
+  ];
 
   beforeAll(async () => {
-    const fixtureContent = await readFile('./test/fixtures/news-fixture.html', {
+    fixtureContent = await readFile('./test/fixtures/news-fixture.html', {
       encoding: 'utf-8',
     });
-    const response = mock<AxiosResponse>({
-      data: fixtureContent,
-      status: 200,
-      statusText: 'OK',
-    });
     httpMock = mockDeep<HttpService>();
-    httpMock.axiosRef.mockResolvedValue(response);
 
     const app: TestingModule = await Test.createTestingModule({
       providers: [
@@ -33,35 +51,21 @@ describe('PostService', () => {
     webScraperService = app.get<WebCrawlerService>(WebCrawlerService);
   });
 
+  beforeEach(() => {
+    const response = mock<AxiosResponse>({
+      data: fixtureContent,
+      status: 200,
+      statusText: 'OK',
+    });
+    httpMock.axiosRef.mockResolvedValue(response);
+  });
+
   describe('scrapePosts', () => {
     it('scrapes the correct amount of posts', async () => {
       const res = await webScraperService.scrapePosts();
 
       expect(res.length).toBe(5);
     });
-
-    const expectedPosts: Post[] = [
-      { number: 1, title: 'AI Coding Is Gambling', points: 58, comments: 36 },
-      { number: 2, title: 'Death to Scroll Fade', points: 227, comments: 113 },
-      {
-        number: 3,
-        title: 'Snowflake AI Escapes Sandbox and Executes Malware',
-        points: 146,
-        comments: 38,
-      },
-      {
-        number: 4,
-        title: 'Show HN: Will my flight have Starlink?',
-        points: 40,
-        comments: 21,
-      },
-      {
-        number: 5,
-        title: 'A tiny, decentralised tool to explore the small web',
-        points: 88,
-        comments: 0,
-      },
-    ];
 
     it(`parses posts correctly`, async () => {
       const res = await webScraperService.scrapePosts();
@@ -73,6 +77,23 @@ describe('PostService', () => {
         expect(points).toBe(expectedPosts[i].points);
         expect(comments).toBe(expectedPosts[i].comments);
       }
+    });
+
+    it('throws an error if no data is returned', async () => {
+      const response = mock<AxiosResponse>({
+        data: undefined,
+        status: 200,
+        statusText: 'OK',
+      });
+      httpMock.axiosRef.mockResolvedValueOnce(response);
+
+      const testFn = async () => {
+        await webScraperService.scrapePosts();
+      };
+
+      await expect(testFn).rejects.toThrow(
+        new Error('Failed to get posts from webpage.'),
+      );
     });
   });
 });
